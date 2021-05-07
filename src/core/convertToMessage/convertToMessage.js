3beb56f0890f93924913f59aa21dec9223f2975f
sajjad <sajjadzibaee102@gmail.com>
2021-05-07 18:26:55 +0430

    fix bug convert

--- src/core/convertToMessage/convertToMessage.js
+++ src/core/convertToMessage/convertToMessage.js
@@ -18,11 +18,12 @@ class convert {
     handel() {
         let message = {};
         message.author = this.client.users.cache.find(us => us.id == this.interaction.member.id)
-        message.guild = this.client.guilds.cache.find(gu => gu.id == this.interactions.guild_id)
-        message.channel = this.client.channels.cache.find(ch => ch.id == this.interactions.channel_id)
-        message.interaction_id = this.interactions.id;
-        message.member = this.interactions.member;
-        message.interaction = this.interactions
+        message.guild = this.client.guilds.cache.find(gu => gu.id == this.interaction.guild_id)
+        message.channel = this.client.channels.cache.find(ch => ch.id == this.interaction.channel_id)
+        message.interaction_id = this.interaction.id;
+        message.member = this.interaction.member;
+        message.content = this.interaction.data?.name || null
+        message.interaction = this.interaction
         return message
     }
 
