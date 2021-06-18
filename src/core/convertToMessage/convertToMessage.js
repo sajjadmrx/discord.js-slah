9926102bf5323e90a193f93b744831f73f9aefbe
sajjad <sajjadzibaee102@gmail.com>
2021-06-18 19:37:33 +0430

    added Embed Message With message.reply

--- src/core/convertToMessage/convertToMessage.js
+++ src/core/convertToMessage/convertToMessage.js
@@ -16,25 +16,60 @@ class convert {
 
 
     handel() {
-       let message = {};
-        message.author = this.client.users.cache.find(us => us.id == this.interaction.member.user.id)
-        message.guild = this.client.guilds.cache.find(gu => gu.id == this.interaction.guild_id)
-        message.channel = this.client.channels.cache.find(ch => ch.id == this.interaction.channel_id)
-        message.interaction_id = this.interaction.id;
-        message.member = this.client.users.cache.find(us => us.id == this.interaction.member.user.id)
-        message.content = this.interaction.data?.name || null
-        message.interaction = this.interaction
-        message.reply = (text) => {
-            this.client.api.interactions(message.interaction.id, message.interaction.token).callback.post({
-                data: {
-                    type: 4,
-                    data: {
-                        content: text
+        try {
+            let message = {};
+            message.author = this.client.users.cache.find(us => us.id == this.interaction.member.user.id)
+            message.guild = this.client.guilds.cache.find(gu => gu.id == this.interaction.guild_id)
+            message.channel = this.client.channels.cache.find(ch => ch.id == this.interaction.channel_id)
+            message.interaction_id = this.interaction.id;
+
+            message.member = message.guild.members.cache.find(us => us.id == this.interaction.member.user.id)
+            message.content = this.interaction.data?.name || null
+            message.interaction = this.interaction
+            message.reply = (item) => {
+
+
+                if (typeof item == 'object') {
+                    if (item.embed) {
+                        this.client.api.interactions(message.interaction.id, message.interaction.token).callback.post({
+                            data: {
+                                type: 4,
+                                data: {
+                                    // content: text
+                                    embeds: [item.embed]
+                                }
+                            }
+                        })
+                    }
+                    else {
+                        this.client.api.interactions(message.interaction.id, message.interaction.token).callback.post({
+                            data: {
+                                type: 4,
+                                data: {
+                                    // content: text
+                                    embeds: [item]
+                                }
+                            }
+                        })
                     }
                 }
-            })
+                else if (typeof item == 'string') {
+                    this.client.api.interactions(message.interaction.id, message.interaction.token).callback.post({
+                        data: {
+                            type: 4,
+                            data: {
+                                content: item
+                            }
+                        }
+                    })
+                }
+                else
+                    throw Error("oops, Not currently supported")
+            }
+            return message
+        } catch (error) {
+            throw new Error(error)
         }
-        return message
     }
 
 
